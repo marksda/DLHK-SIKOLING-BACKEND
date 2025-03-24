6d28991fe0f00d4f422f1230d125c88bd7a3aaf4
marksda <vlack123@gmail.com>
2025-03-24 12:51:27 +0700

    cso

--- DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/RestfulProvider.java
+++ DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/RestfulProvider.java
@@ -1,6 +1,9 @@
 package com.cso.sikolingrestful;
 
-import com.cso.sikoling.abstraction.repository.PropinsiRepository;
+import com.cso.sikoling.abstraction.entity.Filter;
+import com.cso.sikoling.abstraction.entity.Propinsi;
+import com.cso.sikoling.abstraction.entity.QueryParamFilters;
+import com.cso.sikoling.abstraction.repository.Repository;
 import com.cso.sikoling.abstraction.service.alamat.PropinsiService;
 import com.cso.sikoling.abstraction.service.alamat.PropinsiServiceBasic;
 import com.cso.sikoling.main.Infrastructure;
@@ -14,7 +17,7 @@ public class RestfulProvider {
 
     @Produces
     public PropinsiService getPropinsiService(
-            @Infrastructure PropinsiRepository propinsiRepository) {
+            @Infrastructure Repository<Propinsi, QueryParamFilters, Filter> propinsiRepository) {
         return new PropinsiServiceBasic(propinsiRepository);
     }
 }
