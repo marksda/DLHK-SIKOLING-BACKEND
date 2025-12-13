5d51fef8cefabe38b183514c1aaf1db6dcc70ef8
marksda <vlack123@gmail.com>
2025-12-13 12:57:03 +0700

    cso

--- DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/resources/permohonan/KategoriPermohonanResource.java
+++ DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/resources/permohonan/KategoriPermohonanResource.java
@@ -24,6 +24,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import com.cso.sikoling.abstraction.entity.permohonan.KategoriPermohonan;
 import com.cso.sikoling.abstraction.service.Service;
+import com.cso.sikolingrestful.Role;
+import com.cso.sikolingrestful.annotation.RequiredAuthorization;
+import com.cso.sikolingrestful.annotation.RequiredRole;
+import com.cso.sikolingrestful.resources.FilterDTO;
+import java.util.ArrayList;
 
 @Stateless
 @LocalBean
@@ -61,6 +66,8 @@ public class KategoriPermohonanResource {
     }
     
     @POST
+    @RequiredAuthorization
+    @RequiredRole({Role.ADMINISTRATOR})
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public KategoriPermohonanDTO save(KategoriPermohonanDTO kategoriPermohonanDTO) throws SQLException { 
@@ -76,6 +83,8 @@ public class KategoriPermohonanResource {
     
     @Path("/{idLama}")
     @PUT
+    @RequiredAuthorization
+    @RequiredRole({Role.ADMINISTRATOR})
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public KategoriPermohonanDTO update(@PathParam("idLama") String idLama, KategoriPermohonanDTO kategoriPermohonanDTO) throws SQLException {
@@ -96,6 +105,8 @@ public class KategoriPermohonanResource {
     
     @Path("/update_id/{idLama}")
     @PUT
+    @RequiredAuthorization
+    @RequiredRole({Role.ADMINISTRATOR})
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public KategoriPermohonanDTO updateId(@PathParam("idLama") String idLama, KategoriPermohonanDTO kategoriPermohonanDTO) throws SQLException {
@@ -117,6 +128,8 @@ public class KategoriPermohonanResource {
     
     @Path("/{idKategoriPermohonan}")
     @DELETE
+    @RequiredAuthorization
+    @RequiredRole({Role.ADMINISTRATOR})
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public JsonObject delete(@PathParam("idKategoriPermohonan") String idKategoriPermohonan) throws SQLException {
@@ -135,6 +148,44 @@ public class KategoriPermohonanResource {
             throw new IllegalArgumentException("id kategori permohonan harus bilanagan");
         }        
         
+    }
+    
+    @Path("/jumlah")
+    @GET
+    @RequiredAuthorization
+    @RequiredRole({Role.ADMINISTRATOR})
+    @Produces({MediaType.APPLICATION_JSON})
+    public JsonObject getJumlahData(@QueryParam("filters") String qfilters) {
+        try {
+            if(qfilters != null) {
+                Jsonb jsonb = JsonbBuilder.create();
+                List<FilterDTO> filters = jsonb.fromJson(qfilters, new ArrayList<FilterDTO>(){}.getClass().getGenericSuperclass());
+                
+                JsonObject model = Json.createObjectBuilder()
+                    .add(
+                        "jumlah", 
+                        kategoriPermohonanService.getJumlahData(
+                            filters
+                                .stream()
+                                .map(t -> t.toFilter())
+                                .collect(Collectors.toList())
+                        )
+                    )
+                    .build();            
+            
+                return model;
+            }
+            else {
+                JsonObject model = Json.createObjectBuilder()
+                    .add("jumlah", kategoriPermohonanService.getJumlahData(null))
+                    .build();            
+            
+                return model;
+            }
+        }
+        catch (JsonbException e) {
+            throw new JsonbException("format query data json tidak sesuai");
+        }
     }
     
 }
