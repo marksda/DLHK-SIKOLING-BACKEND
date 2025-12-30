b192837d10f148570553ff141090201219b6690f
marksda <vlack123@gmail.com>
2025-12-30 12:26:10 +0700

    cso

---
+++ DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/resources/integrator/WopiResource.java
@@ -0,0 +1,36 @@
+package com.cso.sikolingrestful.resources.integrator;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Stateless
+@LocalBean
+@Path("wopi")
+public class WopiResource {
+    
+    @Path("/files/{id}/content")
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    public String getTesWopi() {
+        return "Hello wopi!!";
+    }
+    
+    @Path("/files/{id}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public JsonObject getCheckFileInfo() {
+        
+        JsonObject model = Json.createObjectBuilder()
+                    .add("BaseFileName", "test.txt")
+                    .add("Size", 11)
+                    .build();            
+            
+        return model;
+    }
+}
