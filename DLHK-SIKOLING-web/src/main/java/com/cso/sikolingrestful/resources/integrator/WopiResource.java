6b401e2f9b97ab97940fb0c138c30de7a5288471
marksda <vlack123@gmail.com>
2026-01-02 06:28:14 +0700

    cso

--- DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/resources/integrator/WopiResource.java
+++ DLHK-SIKOLING-web/src/main/java/com/cso/sikolingrestful/resources/integrator/WopiResource.java
@@ -6,32 +6,60 @@ import jakarta.ejb.Stateless;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Stateless
 @LocalBean
 @Path("wopi")
-public class WopiResource {
+public class WopiResource {    
     
-    @Path("/files/{id}/content")
-    @GET
-    @Produces({MediaType.TEXT_PLAIN})
-    public String getTesWopi() {
-        return "Hello wopi!!";
-    }
-    
-    @Path("/files/{id}")
+    @Path("/files/{file_id}")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     @WopiResponseHeader
-    public JsonObject getCheckFileInfo() {
+    public JsonObject wopiCheckFileInfo(@PathParam("file_id") String file_id) {
         JsonObject model = Json.createObjectBuilder()
                     .add("BaseFileName", "test.txt")
                     .add("Size", 11)
+                    .add("UserId", "user id")
+                    .add("UserCanWrite", true)
                     .build();            
             
         return model;
     }
+    
+    @Path("/files/{file_id}/contents")
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    @WopiResponseHeader
+    public String wopiGetFile(@PathParam("file_id") String file_id) {
+        return "Hello wopi!!";
+    }
+    
+    @Path("/files/{file_id}/contents")
+    @POST
+    @Produces({MediaType.TEXT_PLAIN})
+    @WopiResponseHeader
+    public String wopiPutFile(@PathParam("file_id") String file_id) {
+        return "Hello wopi!!";
+    }
+    
+    @Path("/files/{file_id}")
+    @POST
+    @Produces({MediaType.TEXT_PLAIN})
+    @WopiResponseHeader
+    public String wopiPutRelativeFileOrRenameFile(@PathParam("file_id") String file_id) {
+        //jika request header X-WOPI-Override=PUT_FILE -> buat file baru
+        //dengan nama yang ada pada request header X-WOPI-SuggestedTarget=nama file
+        
+        //jika request header X-WOPI-Override=RENAME_FILE -> rename file
+        //dengan nama yang ada pada request header X-WOPI-RequestedName=nama file
+        
+        return "Hello wopi!!";
+    }
+    
 }
